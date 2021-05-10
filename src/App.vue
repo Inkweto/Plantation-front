c5fe54a6810e3ca4db234e36aa01eea252d60697
Juk3n <adekenn3@gmail.com>
2021-05-10 22:16:05 +0200

    moved project to upper catalogue

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,29 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="green"
       dark
     >
-      <div class="d-flex align-center">
+      <v-btn
+        href="https://github.com/Inkweto/Plantation-front"
+        target="_blank"
+        text
+      >
         <v-img
-          alt="Vuetify Logo"
+          :src="require('./assets/logo.png')"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
           transition="scale-transition"
           width="40"
         />
+        <span class="mr-2">Plantation</span>
+      </v-btn>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/Inkweto/Plantation-front"
         target="_blank"
         text
       >
