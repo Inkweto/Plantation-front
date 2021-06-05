fb45749911c17322a77c814314cd94e7e9c03876
Juk3n <adekenn3@gmail.com>
2021-06-05 10:29:05 +0200

    vue gets plants from server

---
+++ src/components/AddPlantPopup.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-dialog max-width="600px">
+        <template v-slot:activator="{ on }">
+            <v-btn v-on="on" text slot="activator" class="success">Add plant</v-btn>
+        </template>
+        <v-card>
+            <v-card-title>
+                <h3>Add a new plant</h3>
+            </v-card-title>
+            <v-card-text>
+                <v-form class="px-3">
+                    <v-text-field label="Name" v-model="name"></v-text-field>
+                    <v-text-field label="Height" v-model="height"></v-text-field>
+                    <v-text-field label="Diameter" v-model="diameter"></v-text-field>
+                    <v-btn test class="success mx-0 mt-3" v-on:click='addPlant'>Add plant</v-btn>
+                </v-form>
+            </v-card-text>
+        </v-card>
+    </v-dialog>
+</template>
+ 
+<script>
+
+export default {
+  data() {
+      return {
+          name: '',
+          height: '',
+          diameter: '',
+      }
+  },
+  methods: {
+      addPlant() {
+          console.log(this.name, this.height, this.diameter)
+      }
+  }
+}
+</script>
--- src/components/Plants.vue
+++ src/components/Plants.vue
@@ -1,25 +1,18 @@
 <template>
   <div class="plants">
-      
-
     <v-container class="my-5">
       
       <v-row>
         <h1 class="display-1 mb-3 heading dark--text">
           Plants
         </h1>
-        <v-btn
-          rounded
-          color="green"
-          dark
-        >
-          Add plant
-        </v-btn>
       </v-row>
-      
+      <v-flex class="mt-4 mb-3">
+        <AddPlantPopup />
+      </v-flex>
 
       <v-layout row wrap align-content-center>
-        <v-flex xs6 sm4 md3 lg2 v-for="plant in plants" :key="plant.name">
+        <v-flex xs6 sm4 md3 lg2 v-for="plant in plants1" :key="plant.name">
           <v-card flat class="text-xs-center ma-3">
             <v-responsive class="pt-4">
               <v-img 
@@ -43,18 +36,37 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import AddPlantPopup from './AddPlantPopup'
+
 export default {
+  components: { AddPlantPopup },
   data() {
     return {
-      plants: [
+      plants1: [
         { name: 'Orfea Logini', height: '15', diameter: '20', photo_path: 'sample2.jpg' },
         { name: 'Paprotka', height: '50', diameter: '40', photo_path: 'sample1.jpg' },
-        { name: 'Kaktusik', height: '5', diameter: '2', photo_path: 'sample1.jpg' },
-        { name: 'Kaktusik', height: '5', diameter: '2', photo_path: 'sample1.jpg' },
-        { name: 'Kaktusik', height: '5', diameter: '2', photo_path: 'sample1.jpg' },
+        { name: 'Truskawki', height: '5', diameter: '2', photo_path: 'sample1.jpg' },
+        { name: 'Jemio?a', height: '5', diameter: '2', photo_path: 'sample1.jpg' },
+        { name: 'Bocian', height: '5', diameter: '2', photo_path: 'sample1.jpg' },
         { name: 'Kaktusik', height: '5', diameter: '2', photo_path: 'sample1.jpg' },
         { name: 'Pomidorki', height: '10', diameter: '4', photo_path: 'sample2.jpg' },
-      ] 
+      ],
+      plants: [],
+      result: ''
+    }
+  },
+  created() {
+        this.initialize()
+    },
+  methods: {
+    async initialize() {
+      this.result = await fetch(Vue.prototype.$api_url + "/plants/", {
+      headers: {
+        Accept: "application/json"
+      }
+      });
+      this.plants = await this.result.json();
     }
   }
 }
